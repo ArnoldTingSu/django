8443072b4bce10882c3537e0e3a8e21f6677db65
Arnold Su <arnoldtingsu@gmail.com>
2020-11-13 15:49:43 -0800

    11/13/2020

--- dojo_survey/submit_inf/views.py
+++ dojo_survey/submit_inf/views.py
@@ -7,15 +7,6 @@ def index(request):
 def display(request):
     if request.method == 'POST':
         print(request.POST)
-        print(request.POST['name'])
-        request.session['name'] = request.POST['name']
-        print(request.POST['location'])
-        request.session['location'] = request.POST['location']
-        print(request.POST['language'])
-        request.session['language'] = request.POST['language']
-        print(request.POST['comments'])
-        request.session['comments'] = request.POST['comments']
-        
         context = {
             'name':request.POST['name'],
             'location':request.POST['location'],
